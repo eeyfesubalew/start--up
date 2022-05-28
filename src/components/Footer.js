9312349e9bd8d8c7dfd832959e44c0a733e32fcd
eeyfesubalew <esubalew.seife@gmail.com>
2022-05-27 17:06:58 -0800

    footer final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,37 @@
 import "./Footer.scss";
+import { useState } from "react";
+import logo from "../assets/logo.svg";
+const linkDate = [
+  {
+    id: "li1",
+    linkTite: "Quike Link",
+    l1: "Road Map",
+    l2: "Privacy Policy",
+    l3: "Refund Policy",
+    l4: "Terms of Service",
+    l5: "Pricing",
+  },
+  {
+    id: "li2",
+    linkTite: "Resources",
+    l1: "Road Map",
+    l2: "Privacy Policy",
+    l3: "Refund Policy",
+    l4: "Terms of Service",
+    l5: "Pricing",
+  },
+  {
+    id: "li3",
+    linkTite: "Contact Us",
+    l1: "Road Map",
+    l2: "Privacy Policy",
+    l3: "Refund Policy",
+    l4: "Terms of Service",
+    l5: "Pricing",
+  },
+];
 const Footer = () => {
+  const [linkState, SetLinkState] = useState(linkDate);
   return (
     <section className="footer-section">
       <div className="footer-sub">
@@ -13,10 +45,47 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-container">
-        <div className="footer-col"></div>
-        <div className="footer-col"></div>
-        <div className="footer-col"></div>
-        <div className="footer-col"></div>
+        <div className="footer-col footer-col-1">
+          <img src={logo} className="logo" />
+          <p className="footer-text">
+            Lorem ipsum dolor sit amet consetetur sadipscing elitr, sederfs diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam.
+          </p>
+        </div>
+
+        {linkState.map((link) => {
+          return (
+            <div className="footer-col" key={link.id}>
+              <p className="links">{link.linkTite}</p>
+              <ul className="footer-list">
+                <li className="footer-item">
+                  <a href="#">{link.l1}</a>
+                </li>
+                <li className="footer-item">
+                  <a href="#">{link.l2}</a>
+                </li>
+                <li className="footer-item">
+                  <a href="#">{link.l3}</a>
+                </li>
+                <li className="footer-item">
+                  <a href="#">{link.l4}</a>
+                </li>
+                <li className="footer-item">
+                  <a href="#">{link.l5}</a>
+                </li>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+      <div className="copyright">
+        <p> Developed by Seyfe Esubalew</p>
+        <p>
+          Design Credit to{" "}
+          <a className="uni" href="https://uideck.com/" target="_blank">
+            UIdeck
+          </a>
+        </p>
       </div>
     </section>
   );
